4e5e185a9b78611a3988f60eeeb02b840d99dad7
ili <chudinia@gmail.com>
2025-01-10 15:05:01 +0500

    Новые НДС

--- TinkoffPaymentClientApi/Enums/ETax.cs
+++ TinkoffPaymentClientApi/Enums/ETax.cs
@@ -8,24 +8,40 @@
     /// </summary>
     public const string None = "none";
     /// <summary>
-    /// 0%
+    /// НДС по ставке 0%
     /// </summary>
     public const string Vat0 = "vat0";
     /// <summary>
-    /// 10%
+    /// НДС по ставке 10%
     /// </summary>
     public const string Vat10 = "vat10";
     /// <summary>
-    /// 20%
+    /// НДС по ставке 20%
     /// </summary>
     public const string Vat20 = "vat20";
     /// <summary>
-    /// 10/110
+    /// НДС по расчетной ставке 10/110
     /// </summary>
     public const string Vat110 = "vat110";
     /// <summary>
-    /// 20/120
+    /// НДС по расчетной ставке 20/120
     /// </summary>
     public const string Vat120 = "vat120";
+    /// <summary>
+    /// НДС по ставке 5%
+    /// </summary>
+    public const string Vat5 = "vat5";
+    /// <summary>
+    /// НДС по ставке 7%
+    /// </summary>
+    public const string Vat7 = "vat7";
+    /// <summary>
+    /// НДС по расчетной ставке 5/105
+    /// </summary>
+    public const string Vat105 = "vat105";
+    /// <summary>
+    /// НДС по расчетной ставке 7/107
+    /// </summary>
+    public const string Vat107 = "vat107";
   }
 }
